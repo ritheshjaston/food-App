a156c3dd9f2b00762997701f1fb598c6baa0c8c5
Rithesh Jaston D'sa <ritheshjaston22@gmail.com>
2024-02-09 16:18:21 +0530

    express added

--- src/components/Carousal.jsx
+++ src/components/Carousal.jsx
@@ -7,7 +7,7 @@ import '../App.css';
 
 export default function Carousal() {
     return (
-        <div id="carouselExampleControls" className="carousel slide bgsend w-100" data-bs-ride="carousel" >
+        <div id="carouselExampleControls" className="carousel slide bgsend w-100" data-bs-ride="carousel" style={{maxHeight:"700px",objectFit:"contain !important"}}>
             <div className="carousel-inner">
                 <div className="carousel-item active">
                     <img src={img1} className="d-block w-100 " alt="..."/>
