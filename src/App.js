4ef9779d322af3e92cd677321e468a7c96bbd0d9
Rithesh Jaston D'sa <ritheshjaston22@gmail.com>
2024-02-10 23:04:46 +0530

    signup page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Home from './screens/Home';
+import Signup from './screens/Signup';
 import { BrowserRouter,Route,Routes,Link } from 'react-router-dom';
 function App() {
   return (
    <BrowserRouter>
     <Routes>
       <Route exact path='/' element={<Home/>}>
+      </Route>
+      <Route exact path='/signup' element={<Signup/>}>
       </Route>
     </Routes>
    </BrowserRouter>
---
+++ src/screens/Signup.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../my.css'
+export default function Signup() {
+  return (
+    <div>
+      <div className="signupmain container">
+        <h2>Sign Up | Food App</h2>
+            <form action="">
+                <label htmlFor="email">Email</label> <br />
+                <input type="email" name='email' />
+            </form>
+      </div>
+    </div>
+  )
+}
