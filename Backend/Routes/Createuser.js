494325cb50ce6cac42f9d20b2e213b22abc8f955
Rithesh Jaston D'sa <ritheshjaston22@gmail.com>
2024-02-10 20:41:03 +0530

    express validation apply

--- Backend/Routes/Createuser.js
+++ Backend/Routes/Createuser.js
@@ -1,20 +1,30 @@
-const express=require('express');
-const roter=express.Router();
-const user=require('../models/User');
+const express = require("express");
+const roter = express.Router();
+const user = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
-roter.post('/createuser',async (req,res)=>{
-    try {
-       await user.create({
-            name:req.body.name,
-            password:req.body.password,
-            email:req.body.email,
-            location:req.body.location
-        });
-        res.json({success:true});
-    } catch (error) {
-        console.log(error);
-        res.json({success:false});
-
+roter.post(
+  "/createuser",
+  body("email").isEmail(),
+  body("name").isLength({ min: 5 }),
+  body("password").isLength({ min: 5 }),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-});
-module.exports=roter;
+    try {
+      await user.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+        location: req.body.location,
+      });
+      res.json({ success: true });
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false });
+    }
+  }
+);
+module.exports = roter;
