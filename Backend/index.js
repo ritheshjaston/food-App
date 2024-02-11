4eb35f068a250ac26947200ad3f84152fa9c5980
Rithesh Jaston D'sa <ritheshjaston22@gmail.com>
2024-02-11 08:55:24 +0530

    signup a user

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const mongodb =require('./Db')
-mongodb();  
+const express = require("express");
+const app = express();
+const port = 5000;
+const mongodb = require("./Db");
+mongodb();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 
-app.use("/api",require("./Routes/Createuser"));
+app.use("/api", require("./Routes/Createuser"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -1,13 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../my.css'
+import { Link } from 'react-router-dom' 
 export default function Signup() {
+  const [credentials, setcredentials]=useState({name:"",email:"",password:"",geolocation:""});
+
+  const handlesubmit= async(e)=>{
+      e.preventDefault();
+      const response=await fetch("http://localhost:5000/api/createuser",{
+        method:'POST',
+        headers:{
+          'Content-type':'Application/json'
+        },
+        body:JSON.stringify({
+          name:credentials.name,
+          email:credentials.email,
+          password:credentials.password,
+          location:credentials.geolocation
+        })
+    })
+    const json = await response.json();
+    console.log(json);
+
+    if(!json.success){
+      alert("Enter valid credentials");
+    }
+  }
+  const change=(event)=>[
+    setcredentials({...credentials,[event.target.name]:event.target.value})
+  ]
   return (
     <div>
       <div className="signupmain container">
         <h2>Sign Up | Food App</h2>
-            <form action="">
-                <label htmlFor="email">Email</label> <br />
-                <input type="email" name='email' />
+        <br />
+        <br />
+            <form onSubmit={handlesubmit}>
+                <label htmlFor="email" className='formlabel'>Email</label> <br />
+                <input type="email" name='email' className='input' value={credentials.email} onChange={change}/>
+                <br />
+                <label htmlFor="email" className='formlabel'>Password</label> <br />
+                <input type="password" name='password' className='input' value={credentials.password} onChange={change}/>
+                <br />
+                <label htmlFor="name" className='formlabel'>Name</label> <br />
+                <input type="text" name='name' className='input' value={credentials.name} onChange={change}/>
+                <br />
+                <label htmlFor="location" className='formlabel'>Location</label> <br />
+                <input type="text" name='geolocation' className='input' value={credentials.geolocation} onChange={change}/>
+                <button type="submit" className="btn btn-outline-primary buttonform">Signup now</button>
+                <br />
+                <Link to="/login" className="btn btn-outline-danger buttonalreadyuser" >Already a user</Link>
+                <br />
+               
             </form>
       </div>
     </div>
