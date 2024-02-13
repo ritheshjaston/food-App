bfd1cf3a764279b0e200d1174c8d84b5d25e9dc3
Rithesh Jaston D'sa <ritheshjaston22@gmail.com>
2024-02-13 09:32:35 +0530

    get data from front end

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
@@ -6,7 +6,24 @@ import Carousal from "../components/Carousal";
 import Searchbar from "../components/Searchbar";
 import '../App.css';
 export default function Home() {
-  
+    const [foodcat,setfoodcat]=useState([]);
+    const [fooditems,setfooditems]=useState([]);
+
+    const loaddata= async ()=>{
+      let res=await fetch("http://localhost:5000/api/fooddata",{
+        method:'POST',
+          headers:{
+            'Content-type':'Application/json'
+          }
+      });
+      res=await res.json();
+      console.log(res[0],res[1]);
+    }
+
+    useEffect(()=>{
+      loaddata();
+    },[]);
+
   return (
     <div>
       <div>
